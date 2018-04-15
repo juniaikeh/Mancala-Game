41bdefd1b16ce1bdc68e223e2d8cfcc32d03c9fd
Dev Kapupara <devkapupara@gmail.com>
2018-04-14 21:50:53 -0700

    Skeleton

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -1,4 +1,14 @@
+import javax.swing.*;
+
 public class MancalaTest
 {
-
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Mancala Game");
+        frame.setSize(800,400);
+        frame.setResizable(false);
+        frame.add(new Views(), "Center");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
--- src/Views.java
+++ src/Views.java
@@ -1,4 +1,29 @@
-public class Views
-{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.RoundRectangle2D;
 
+public class Views extends JPanel
+{
+    private static final int BOARD_WIDTH = 700;
+    private static final int BOARD_HEIGHT = 300;
+    private JPanel player1 = new JPanel();
+    private JPanel player2 = new JPanel();
+
+    Views()
+    {
+        add(player1, "West");
+        add(player2, "East");
+    }
+
+//    public void paintComponent(Graphics g)
+//    {
+//        Graphics2D g2 = (Graphics2D)g;
+//        Container parent = getParent();
+//        int x = (parent.getWidth()-BOARD_WIDTH)/2;
+//        int y = (parent.getHeight()-BOARD_HEIGHT)/2;
+//        RoundRectangle2D boundary = new RoundRectangle2D.Double(x, y, BOARD_WIDTH,BOARD_HEIGHT,100,100);
+//        g2.setStroke(new BasicStroke(5));
+//        g2.draw(boundary);
+//    }
 }
