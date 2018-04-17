6d614670579d23e995c5174e3bee17e3eaff9ce2
Dev Kapupara <devkapupara@gmail.com>
2018-04-17 15:33:48 -0700

    Pits inside ArrayList for accessing

--- src/Views.java
+++ src/Views.java
@@ -31,17 +31,17 @@ public class Views extends JPanel
         double pitWidth = BOARD_WIDTH / 10;
         double pitHeight = BOARD_HEIGHT / 3;
 
-        pits.add(new Ellipse2D.Double(x+(topX*7) , y+ OFFSET_FROM_BORDER/2 , BOARD_WIDTH / 10, BOARD_HEIGHT-OFFSET_FROM_BORDER ));       // Add Player 1 Mancala at index 0
+        pits.add(new Ellipse2D.Double(x+(topX*7) , y+ OFFSET_FROM_BORDER/2 , pitWidth, BOARD_HEIGHT-OFFSET_FROM_BORDER ));       // Add Player 1 Mancala at index 0
 
         for (int i = 1; i < 7; i++)                                                 // Top Row Pits
             pits.add(new Ellipse2D.Double(x+(topX*i)+10, baseY, pitWidth, pitHeight));
 
-        baseY = y+((BOARD_HEIGHT*2)/3)-OFFSET_FROM_BORDER;
+        baseY = y+((BOARD_HEIGHT*2)/3)-OFFSET_FROM_BORDER;                          // Change y for bottom pits.
 
         for (int i = 1; i < 7; i++)                                                 // Bottom Row Row Pits
             pits.add(new Ellipse2D.Double(x+(topX*i)+10, baseY, pitWidth, pitHeight));
 
-        pits.add(new Ellipse2D.Double(x+20 , y+OFFSET_FROM_BORDER/2, BOARD_WIDTH / 10, BOARD_HEIGHT-OFFSET_FROM_BORDER));               // Add Player 2 Mancala at index 13.
+        pits.add(new Ellipse2D.Double(x+20 , y+OFFSET_FROM_BORDER/2, pitWidth, BOARD_HEIGHT-OFFSET_FROM_BORDER));               // Add Player 2 Mancala at index 13.
 
         for (Ellipse2D e: pits)
             g2.draw(e);
